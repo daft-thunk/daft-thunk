d28b59c66913107e089ec2ff18ee89af4d9e1b25
Josh <carlson.joshuaph@gmail.com>
2018-02-26 15:32:27 -0600

    products page started

---
+++ client/components/Products.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+export function Products(props){
+  return (
+    <div>
+    <h1> all the products</h1>
+      {
+        //logic to not run for now
+        1 === 11 && props.products.map(product => {
+          return (
+            <h1 key={product.id}>{product.name}</h1>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+const mapProps = state => ({
+  products: state.products
+})
+
+const Container = connect(mapProps)(Products)
+
+export default Container
+
---
+++ client/store/products.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const INIT_PRODUCTS = 'INIT PRODUCTS'
+
+/**
+ * ACTION CREATORS
+ */
+const initProducts = (products) => ({type: INIT_PRODUCTS, products})
+
+/**
+ * THUNK CREATORS
+ */
+export const getProductsThunk = () =>
+  dispatch =>
+    axios.get('/api/products')
+      .then(res =>
+        dispatch(initProducts(res.data)))
+      .catch(err => console.log(err))
+
+/**
+ * REDUCER
+ */
+export default function (state = [], action) {
+  switch (action.type) {
+    case INIT_PRODUCTS:
+      return action.products
+    default:
+      return state
+  }
+}
