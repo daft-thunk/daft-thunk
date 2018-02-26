369b9cea934a073f8144fcbc94a1d1f625972a30
Josh <carlson.joshuaph@gmail.com>
2018-02-26 17:32:07 -0600

    search bars set up

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Products} from './components'
+import {Login, Signup, UserHome, Products, SingleProduct} from './components'
 import {me} from './store'
 
 /**
@@ -21,7 +21,8 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route path="/products" component={Products} />
+        <Route exact path="/products" component={Products} />
+        <Route path="/products/:id" component={SingleProduct} />
         {
           isLoggedIn &&
             <Switch>
