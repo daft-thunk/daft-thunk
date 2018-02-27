3c3aeab78f90be46b724b32fe28c0dd28f9c2d09
Josh <carlson.joshuaph@gmail.com>
2018-02-27 14:48:43 -0600

    homepage featured cards added

--- client/components/index.js
+++ client/components/index.js
@@ -13,4 +13,5 @@ export {default as ProductSelector} from './ProductSelector';
 export {default as Cart} from './cart';
 export {default as Home} from './Home';
 export {default as Carousel} from './Carousel';
+export {default as ProductCard} from './ProductCard';
 // export { Cart } from './Cart'
