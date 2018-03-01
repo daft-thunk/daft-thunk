22f6cbf82f7a33d61c85cffcedf2a7c6a9535693
Josh <carlson.joshuaph@gmail.com>
2018-03-01 17:35:46 -0600

    cookie almost done

--- client/index.js
+++ client/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {Router} from 'react-router-dom';
 import history from './history';
-import store, { increment } from './store';
+import store, { increment, fetchCart } from './store';
 import App from './app';
+import axios from 'axios';
 
 
 // establishes socket connection
 import './socket';
+
+// function setCookie(cname, cvalue, exdays) {
+//   var d = new Date();
+//   d.setTime(d.getTime() + (exdays*24*60*60*1000));
+//   var expires = "expires="+ d.toUTCString();
+//   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// }
+
+///set up cookie logic
+// if (!document.cookie) {
+//   axios.post('/api/cart')
+//   .then(res => res.data)
+//   .then(cart => {
+//     document.cookie = cart.id
+//     store.dispatch(fetchCart(cart.id))
+//   })
+//}
 
 ReactDOM.render(
   <Provider store={store}>
