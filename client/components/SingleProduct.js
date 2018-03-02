513b0ccfe0289d6201e609e7bcc11469a597af85
Dave Cohen <dcohen18@gmail.com>
2018-03-02 10:56:23 -0600

    add review form to single product

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,38 +1,47 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { history } from '../history';
 import store, { setProductThunk } from '../store';
-import { Icon, Button } from 'semantic-ui-react'
+import { Icon, Button } from 'semantic-ui-react';
+import { Review } from './index';
 
-class SingleProduct extends Component{
-
-  componentWillMount(){
+class SingleProduct extends Component {
+  componentWillMount() {
     store.dispatch(setProductThunk(this.props.match.params.id));
   }
 
-  render(){
+  render() {
     return (
       <div id="singleProductContainer">
         <h1>{this.props.product.name}</h1>
         <hr width="75%" />
         <div className="singleProductflex">
-          <div><img src={this.props.product.imageUrl} /></div>
+          <div>
+            <img src={this.props.product.imageUrl} />
+          </div>
           <div className="productInfo">
-          <h1><Icon name="dollar" />{this.props.product.price}</h1>
-          <h3>{this.props.product.description}</h3>
-          <h3>Manufacturer: {this.props.product.manufacturer}</h3>
-          <Button className="productButton" fluid animated="vertical">
-            <Button.Content hidden>Add to Cart</Button.Content>
+            <h1>
+              <Icon name="dollar" />
+              {this.props.product.price}
+            </h1>
+            <h3>{this.props.product.description}</h3>
+            <h3>Manufacturer: {this.props.product.manufacturer}</h3>
+            <Button className="productButton" fluid animated="vertical">
+              <Button.Content hidden>Add to Cart</Button.Content>
               <Button.Content visible>
-              <Icon name="shop" />
-            </Button.Content>
-           </Button>
+                <Icon name="shop" />
+              </Button.Content>
+            </Button>
           </div>
         </div>
         <hr width="75%" />
         <h1>Reviews</h1>
         {
-          this.props.product.reviews && this.props.product.reviews.map(review => {
+          // check if user has ordered product
+        }
+        <Review />
+        {this.props.product.reviews &&
+          this.props.product.reviews.map(review => {
             let stars = String.fromCharCode(9733).repeat(review.rating);
             return (
               <div key={review.id}>
@@ -41,10 +50,8 @@ class SingleProduct extends Component{
                 <h3>{review.text}</h3>
                 <hr width="50%" />
               </div>
-              );
-            }
-          )
-        }
+            );
+          })}
       </div>
     );
   }
@@ -57,4 +64,3 @@ const mapProps = state => ({
 const Container = connect(mapProps)(SingleProduct);
 
 export default Container;
-
