8191ce87f01d29902b23e0c94d51bafa5ad66ccd
Dave Cohen <dcohen18@gmail.com>
2018-03-03 14:28:15 -0600

    orders filter functional

--- client/components/Orders.js
+++ client/components/Orders.js
@@ -5,28 +5,53 @@ import { connect } from 'react-redux';
 import { getOrdersThunk } from '../store/orders';
 import store from '../store';
 import { Button, Form } from 'semantic-ui-react';
-// import { ProductSearch, ProductSelector, ProductCard } from './index';
 
 class Orders extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredOrders: [],
+      status: 'All'
+    };
+  }
+
   componentDidMount() {
     this.props.fetchOrders();
   }
 
+  handleChange = (evt) => {
+    evt.preventDefault();
+    const status = evt.target.value;
+    if (status === 'All') {
+      this.setState({filteredOrders: [], status});
+    } else {
+      const filteredOrders = this.props.orders.filter(order => {
+        return order.status === status;
+      });
+      this.setState({filteredOrders, status}, () => {
+        // console.log(this.state);
+      });
+    }
+  }
+
   render() {
     let allOrders = this.props.orders;
-    console.log('all orders:', allOrders, this.props);
+    let filteredOrders = this.state.filteredOrders;
+    const ordersToDisplay = filteredOrders.length ? filteredOrders : allOrders;
 
     return (
       <div>
-        <h1>all the orders - admin view</h1>
-        <Form.Field label="Filter orders by category:" control="select">
+        <h1>Orders: Admin Panel</h1>
+        <h3>{this.state.status} orders</h3>
+        <Form.Field label="Filter orders by category:" control="select" onChange={this.handleChange}>
+          <option value="All">All</option>
           <option value="Created">Created</option>
           <option value="Processing">Processing</option>
           <option value="Completed">Completed</option>
           <option value="Cancelled">Cancelled</option>
         </Form.Field>
         <div className="flex">
-          {allOrders.map(order => {
+          {ordersToDisplay.map(order => {
             if (order.purchasedCart) {
               console.log('cart', order.purchasedCart);
             }
@@ -48,40 +73,6 @@ class Orders extends Component {
     );
   }
 }
-
-// export function Orders(props) {
-//     let userOrders = props.orders ? props.orders : [];
-//     console.log(userOrders, props);
-//     if (props.userId) {
-//       // NOT a secure way to filter...
-//       const userId = props.userId;
-//       userOrders = userOrders.filter(order => {
-//         return order.userId === userId;
-//       });
-//     }
-
-//     return (
-//       <div>
-//         <h1>all the orders - admin view</h1>
-//         <div className="flex">
-//           {userOrders.map(order => {
-//             return (
-//               <ul key={order.id}>
-//                 <li>User Id: {order.userId}</li>
-//                 <li>User Email: {order.email}</li>
-//                 <li>Status: {order.status}</li>
-//                 <li>Cart Id:{order.cartId}</li>
-//                 <li>Date Ordered: {order.dateOrdered}</li>
-//                 <li>Date Shipped: {order.dateShipped}</li>
-//                 <li>Date Arrived: {order.dateArrived}</li>
-//                 <li>Mailing Address: {order.mailingAddress}</li>
-//               </ul>
-//             );
-//           })}
-//         </div>
-//       </div>
-//     );
-// }
 
 const mapProps = state => ({
   orders: state.orders
