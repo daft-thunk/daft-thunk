d9e02d63f459b4cecfbdfc108997749e8c7467d1
Dave Cohen <dcohen18@gmail.com>
2018-03-04 15:46:53 -0600

    Add delete user to redux store

--- client/store/users.js
+++ client/store/users.js
@@ -5,12 +5,14 @@ import history from '../history';
  */
 const GET_USERS = 'GET_USERS';
 const CHANGE_USER = 'CHANGE_USER';
+const DELETE_USER = 'DELETE_USER';
 
 /**
  * ACTION CREATORS
  */
 export const getUsers = users => ({ type: GET_USERS, users });
 export const changeUser = user => ({ type: CHANGE_USER, user });
+export const deleteUser = userId => ({ type: DELETE_USER, userId });
 
 /**
  * THUNK CREATORS
@@ -30,6 +32,13 @@ export const changeUserThunk = (user) => dispatch => {
     .catch(err => console.error(`Updating user: ${user} unsuccessful`, err));
 };
 
+export const deleteUserThunk = (userId) => dispatch => {
+  return axios
+    .delete(`/api/users/${userId}`)
+    .then(res => dispatch(deleteUser(userId)))
+    .catch(err => console.error(`Deleting user id #${userId} unsuccessful`, err));
+};
+
 /**
  * REDUCER
  */
@@ -37,6 +46,8 @@ export default function(state = [], action) {
   switch (action.type) {
     case GET_USERS:
       return action.users;
+    case DELETE_USER:
+      return state.filter(user => user.id !== action.userId);
     default:
       return state;
   }
