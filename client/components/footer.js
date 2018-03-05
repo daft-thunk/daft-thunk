cf61e1d7a10359b4705fe85a4e44e2c05f6d4073
Josh <carlson.joshuaph@gmail.com>
2018-03-05 14:57:43 -0600

    footer functional

--- client/components/footer.js
+++ client/components/footer.js
@@ -1,5 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
 
 export default function Footer(){
-
+  return (
+    <div className="footer">
+    <div id="company-info">
+    <Icon name="copyright" /> Daft Thunk Boiz
+    </div>
+    <div id="socials">
+    <a
+    target="_blank"
+    rel="noopener noreferrer"
+    href="https://www.facebook.com/daft.thunk.9"><Icon link name="facebook official" /> </a>
+    <a
+    target="_blank"
+    rel="noopener noreferrer"
+    href="https://www.twitch.tv/daftthunk"><Icon link name="twitch" />
+    </a>
+    <a
+    target="_blank"
+    rel="noopener noreferrer"
+    href="https://twitter.com/daft_thunk"><Icon link name="twitter square" /></a>
+    <a
+    target="_blank"
+    rel="noopener noreferrer"
+    href="https://github.com/daft-thunk"><Icon link name="github" /></a>
+    </div>
+  </div>
+  );
 }
