c77d3ed00e5ae2595fcd918bdc0ece481adce23e
jakeBergs <jake.bergal@gmail.com>
2018-03-06 14:12:22 -0600

    finished category reducer

--- server/api/categories.js
+++ server/api/categories.js
@@ -5,11 +5,11 @@ module.exports = router;
 router.post('/', (req, res, next) => {
   Category.create(req.body)
     .then(category => res.json(category))
-    .catch(console.error);
+    .catch(next);
 });
 
 router.get('/', (req, res, next) => {
   Category.findAll()
     .then(categories => res.json(categories))
-    .catch(console.error);
+    .catch(next);
 });
